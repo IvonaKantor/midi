697d20278c1943d3894f8b080f9ed2c440a6a407
Ivonidze <119894806+IvonaKantor@users.noreply.github.com>
2024-12-21 22:05:18 +0100

    Update MyDrawPanel.java

--- src/main/java/org/example/MyDrawPanel.java
+++ src/main/java/org/example/MyDrawPanel.java
@@ -18,6 +18,7 @@ public class MyDrawPanel extends JPanel{
         g.fillOval(70, 70, 100, 100);
     }
 
+    
     public void Graphics2D(Graphics g){
         Graphics2D g2d = (Graphics2D) g;
 
@@ -25,5 +26,25 @@ public class MyDrawPanel extends JPanel{
         
         g2d.setPaint(gradient);
         g2d.fillOval(70, 70, 100, 100);
+    }
+
+
+    public void Graphics2D_2(Graphics g){
+        Graphics2D g2d = (Graphics2D) g;
+        
+        int red = (int) (Math.random() * 255);
+        int green = (int) (Math.random() * 255);
+        int blue = (int) (Math.random() * 255);
+        Color startcolor = new Color(red, green, blue);
+        
+        red = (int) (Math.random() * 255);
+        green = (int) (Math.random() * 255);
+        blue = (int) (Math.random() * 255);
+        Color endcolor = new Color(red, green, blue);
+        
+        GradientPaint gradient = new GradientPaint(70, 70, startcolor, 150, 150, endcolor);
+        g2d.setPaint(gradient);
+        g2d.fillOval(70, 70, 100, 100);
+        
     }
 }
