4b9541c50c14b7d1008e962a5d1a6e21bb553b83
Ivonidze <119894806+IvonaKantor@users.noreply.github.com>
2024-12-22 17:57:28 +0100

    Delete src/main/java/org/example/Foo.java

--- src/main/java/org/example/Foo.java
+++
@@ -1,8 +0,0 @@
-package org.example.outer_inner;
-
-public class Foo {
-    public static void main(String[] argc){
-        MyOuterClass outerobj = new MyOuterClass();
-        MyOuterClass.MyIneerClass innerobj = outerobj.new MyIneerClass();
-    }
-}
