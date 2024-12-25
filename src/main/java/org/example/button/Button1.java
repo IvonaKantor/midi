5e36783907bdc8cb409f103e68113a530a52642b
Ivonidze <119894806+IvonaKantor@users.noreply.github.com>
2024-12-25 19:50:31 +0100

    Update Button1.java
    tests of different button areas

--- src/main/java/org/example/button/Button1.java
+++ src/main/java/org/example/button/Button1.java
@@ -9,10 +9,18 @@ public class Button1 {
         gui.go();
     }
 
-    public void go(){
+    public void go_east(){
         JFrame frame = new JFrame();
         JButton button = new JButton("click me");
         frame.getContentPane().add(BorderLayout.EAST, button);
+        frame.setSize(200,200);
+        frame.setVisible(true);
+    }
+
+    public void go_north(){
+        JFrame frame = new JFrame();
+        JButton button = new JButton("click me");
+        frame.getContentPane().add(BorderLayout.NORTH, button);
         frame.setSize(200,200);
         frame.setVisible(true);
     }
