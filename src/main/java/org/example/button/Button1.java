d0dabae2730797a58d8ed7f926ba95565ebc3073
Ivonidze <119894806+IvonaKantor@users.noreply.github.com>
2024-12-25 20:03:15 +0100

    Update Button1.java
    Demonstration of frame components

--- src/main/java/org/example/button/Button1.java
+++ src/main/java/org/example/button/Button1.java
@@ -6,6 +6,9 @@ public class Button1 {
 
     public static void main(String[] argc){
         Button1 gui = new Button1();
+        gui.go_east();
+        gui.go_north();
+        gui.go_higher();
         gui.go();
     }
 
@@ -32,6 +35,25 @@ public class Button1 {
         button.setFont(bigFont);
         frame.getContentPane().add(BorderLayout.NORTH, button);
         frame.setSize(200,200);
+        frame.setVisible(true);
+    }
+
+    public void go(){
+        JFrame frame = new JFrame();
+
+        JButton east = new JButton("East");
+        JButton west = new JButton("West");
+        JButton north = new JButton("North");
+        JButton south = new JButton("South");
+        JButton center = new JButton("Center");
+
+        frame.getContentPane().add(BorderLayout.EAST, east);
+        frame.getContentPane().add(BorderLayout.WEST, west);
+        frame.getContentPane().add(BorderLayout.NORTH, north);
+        frame.getContentPane().add(BorderLayout.SOUTH, south);
+        frame.getContentPane().add(BorderLayout.CENTER, center);
+
+        frame.setSize(300,300);
         frame.setVisible(true);
     }
 }
