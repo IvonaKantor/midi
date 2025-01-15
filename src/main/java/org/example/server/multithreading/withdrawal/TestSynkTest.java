634ab0af9b0f1de60897d508ebb798cdc8623003
ivona <ik87056@student.uwb.edu.pl>
2025-01-15 17:43:12 +0100

    problem of last updating

---
+++ src/main/java/org/example/server/multithreading/withdrawal/TestSynkTest.java
@@ -0,0 +1,11 @@
+package org.example.server.multithreading.withdrawal;
+
+public class TestSynkTest {
+    public static void main(String[] args) {
+        TestSync job = new TestSync();
+        Thread a = new Thread(job);
+        Thread b = new Thread(job);
+        a.start();
+        b.start();
+    }
+}
