28388eb3945939f5951579f0d97920487a0d4c1b
ivona <ik87056@student.uwb.edu.pl>
2025-01-16 18:35:20 +0100

    updates of clientchat

--- src/main/java/org/example/server/multithreading/server/SimpleChatClient.java
+++ src/main/java/org/example/server/multithreading/server/SimpleChatClient.java
@@ -64,7 +64,15 @@ public class SimpleChatClient {
 
     public class IncomingReader implements Runnable {
         public void run() {
-
+            String message;
+            try{
+                while((message = reader.readLine()) != null){
+                    System.out.println("read " + message);
+                    incoming.append(message + "\r\n");
+                }
+            }catch(Exception ex) {
+                ex.printStackTrace();
+            }
         }
     }
 
