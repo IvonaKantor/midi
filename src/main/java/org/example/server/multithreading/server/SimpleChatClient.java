52825386ee80b5a047da2056047e9e55d7aed571
ivona <ik87056@student.uwb.edu.pl>
2025-01-16 18:30:09 +0100

    updates of clientchat

--- src/main/java/org/example/server/multithreading/server/SimpleChatClient.java
+++ src/main/java/org/example/server/multithreading/server/SimpleChatClient.java
@@ -1,12 +1,11 @@
 package org.example.server.multithreading.server;
 import java.io.*;
 import java.net.*;
-import java.util.*;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class SimpleChatClientA {
+public class SimpleChatClient {
 
     JTextArea incoming;
     JTextField outgoing;
@@ -21,13 +20,19 @@ public class SimpleChatClientA {
         incoming.setLineWrap(true);
         incoming.setWrapStyleWord(true);
         incoming.setEditable(false);
+        JScrollPane qScroller = new JScrollPane(incoming);
+        qScroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        qScroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
         outgoing = new JTextField(20);
         JButton sendButton = new JButton("Send");
         sendButton.addActionListener(new SendButtonListener());
+        mainPanel.add(qScroller);
         mainPanel.add(outgoing);
         mainPanel.add(sendButton);
-        frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
         setUpNetworking();
+        Thread readerThread = new Thread(new IncomingReader());
+        readerThread.start();
+        frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
         frame.setSize(400,500);
         frame.setVisible(true);
     }
@@ -35,6 +40,8 @@ public class SimpleChatClientA {
     public void setUpNetworking() {
         try{
             sock = new Socket("127.0.0.1", 5000);
+            InputStreamReader streamReader = new InputStreamReader(sock.getInputStream());
+            reader = new BufferedReader(streamReader);
             writer = new PrintWriter(sock.getOutputStream());
             System.out.println("networking established");
         }catch(IOException e){
@@ -55,8 +62,14 @@ public class SimpleChatClientA {
         }
     }
 
+    public class IncomingReader implements Runnable {
+        public void run() {
+
+        }
+    }
+
     public static void main(String[] args) {
-        SimpleChatClientA client = new SimpleChatClientA();
+        SimpleChatClient client = new SimpleChatClient();
         client.go();
     }
 }
