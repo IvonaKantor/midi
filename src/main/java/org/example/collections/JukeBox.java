03a01d6ea034568d8b43552737c9dc55257f286c
ivona <ik87056@student.uwb.edu.pl>
2025-01-28 17:30:51 +0100

    updating JukeBox using comparable interface

--- src/main/java/org/example/collections/JukeBox.java
+++ src/main/java/org/example/collections/JukeBox.java
@@ -3,7 +3,7 @@ import java.util.*;
 import java.io.*;
 
 public class JukeBox {
-    ArrayList<String> songList = new ArrayList<>();
+    ArrayList<Song> songList = new ArrayList<Song>();
 
     public static void main(String[] args){
         new JukeBox().go();
@@ -30,6 +30,7 @@ public class JukeBox {
 
     void addSong(String lineToParse){
         String[] tokens = lineToParse.split("/");
-        songList.add(tokens[0]);
+        Song nextSong = new Song(tokens[0], tokens[1], tokens[2], tokens[3]);
+        songList.add(nextSong);
     }
 }
