d6d8009355c1c4300c333e38f11de1a225372892
ivona <ik87056@student.uwb.edu.pl>
2025-02-07 21:33:36 +0100

    realization equals ans hashcode methods

--- src/main/java/org/example/collections/Song.java
+++ src/main/java/org/example/collections/Song.java
@@ -6,6 +6,15 @@ public class Song implements Comparable<Song>{
     String rating;
     String bpm;
 
+    public boolean equals(Object aSong){
+        Song s = (Song) aSong;
+        return getTitle().equals(s.getTitle());
+    }
+
+    public int hashCode(){
+        return getTitle().hashCode();
+    }
+
     public int compareTo(Song o) {
         return title.compareTo(o.getTitle());
     }
