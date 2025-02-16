dc415a6d2f65806be1dbf587a861072e754ef872
ivona <ik87056@student.uwb.edu.pl>
2025-02-16 17:25:50 +0100

    linkedlist and using comparator in methods

---
+++ src/main/java/org/example/collections/sortMountains/Mountain.java
@@ -0,0 +1,15 @@
+package org.example.collections.sortMountains;
+
+public class Mountain {
+    String name;
+    int height;
+    
+    Mountain(String name, int length) {
+        this.name = name;
+        this.height = length;
+    }
+    
+    public String toString() {
+        return "Mountain [name=" + name + ", length=" + height + "]";
+    }
+}
