95e0d76fc6f61686c971af6a23131f88a547ce6a
ivona <ik87056@student.uwb.edu.pl>
2025-02-16 17:26:44 +0100

    class with sorting and comparing methods

---
+++ src/main/java/org/example/collections/sortMountains/Sort.java
@@ -0,0 +1,38 @@
+package org.example.collections.sortMountains;
+import java.util.*;
+
+public class Sort {
+
+    LinkedList<Mountain> mtn = new LinkedList<Mountain>();
+
+    class NameCompare implements Comparator<Mountain> {
+        public int compare(Mountain mo1, Mountain mo2) {
+            return mo1.toString().compareTo(mo2.toString());
+        }
+    }
+
+    class HeightCompare implements Comparator<Mountain> {
+        public int compare(Mountain mo1, Mountain mo2) {
+            return (mo2.height - mo2.height);
+        }
+    }
+
+    public static void main(String[] args) {
+        new Sort().go();
+    }
+
+    public void go() {
+        mtn.add(new Mountain("Long Range", 14255));
+        mtn.add(new Mountain("Elbert", 14433));
+        mtn.add(new Mountain("Maroon", 14156));
+        mtn.add(new Mountain("Castle", 14265));
+
+        System.out.println("Mountains in ascending order:\n" + mtn);
+        NameCompare nc = new NameCompare();
+        Collections.sort(mtn, nc);
+        System.out.println("Mountains in descending order:\n" + mtn);
+        HeightCompare hc = new HeightCompare();
+        Collections.sort(mtn, hc);
+        System.out.println("Mountains in ascending order:\n" + mtn);
+    }
+}
