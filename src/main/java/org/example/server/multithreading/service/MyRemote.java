1e0aed6d8a99284e4bc2c1d0886796bc9b255e3b
ivona <ik87056@student.uwb.edu.pl>
2025-02-18 20:26:50 +0100

    creating a remote interface

--- src/main/java/org/example/server/multithreading/service/MyRemote.java
+++ src/main/java/org/example/server/multithreading/service/MyRemote.java
@@ -1,6 +1,8 @@
 package org.example.server.multithreading.service;
 
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 public interface MyRemote extends Remote {
+    public String hello() throws RemoteException;
 }
