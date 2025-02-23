65d599b574db71b9211a568d39a380f248fcefe2
ivona <ik87056@student.uwb.edu.pl>
2025-02-23 20:06:04 +0100

    complited servlet

--- src/main/java/org/example/server/multithreading/servlets/MyServlet.java
+++ src/main/java/org/example/server/multithreading/servlets/MyServlet.java
@@ -5,5 +5,19 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
-public class MyServlet extends HttpServlet { }
+public class MyServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+        String message = "If you read that, it means that the servlet works";
+
+        out.println("<HTML><BODY>");
+        out.println("<H1>" + message + "</H1>");
+        out.println("</BODY></HTML>");
+        out.close();
+    }
+}
