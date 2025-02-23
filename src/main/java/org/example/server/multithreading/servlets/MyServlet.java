4c0436bebab7ad5bf0b569b64138a58d954ce5d8
ivona <ik87056@student.uwb.edu.pl>
2025-02-23 19:45:29 +0100

    create my own servlet

---
+++ src/main/java/org/example/server/multithreading/servlets/MyServlet.java
@@ -0,0 +1,9 @@
+package org.example.server.multithreading.servlets;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class MyServlet extends HttpServlet { }
