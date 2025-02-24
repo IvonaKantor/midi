af94f4ff4bc3f13232f5be72cb6302cf5e602ad3
ivona <ik87056@student.uwb.edu.pl>
2025-02-24 22:59:41 +0100

    added actions to the rmi interface

--- src/main/java/org/example/server/multithreading/servlets/ServiceServer.java
+++ src/main/java/org/example/server/multithreading/servlets/ServiceServer.java
@@ -1,5 +1,8 @@
 package org.example.server.multithreading.servlets;
 import java.rmi.*;
+import java.security.Provider;
 
 public interface ServiceServer extends Remote {
+    Object[] getServices() throws RemoteException;
+    Provider.Service getService(Object name) throws RemoteException;
 }
