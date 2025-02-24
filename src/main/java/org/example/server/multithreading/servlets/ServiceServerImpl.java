b77839f6a9d3bd6bce348570054d90141cd1073a
ivona <ik87056@student.uwb.edu.pl>
2025-02-25 00:53:36 +0100

    using hashmap for saving services

--- src/main/java/org/example/server/multithreading/servlets/ServiceServerImpl.java
+++ src/main/java/org/example/server/multithreading/servlets/ServiceServerImpl.java
@@ -3,4 +3,15 @@ import java.rmi.*;
 import java.util.*;
 import java.rmi.server.*;
 
-public class ServiceServerImpl extends UnicastRemoteObject implements ServiceServer { }
+public class ServiceServerImpl extends UnicastRemoteObject implements ServiceServer {
+
+    HashMap serviceList;
+
+    public ServiceServerImpl() throws RemoteException {
+        setUpServices();
+    }
+
+    private void setUpServices() throws RemoteException {
+        serviceList = new HashMap();
+    }
+}
