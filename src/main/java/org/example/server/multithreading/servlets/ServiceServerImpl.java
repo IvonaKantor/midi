3228a89833ade356752999dad316ceaecf2cbfc0
ivona <ik87056@student.uwb.edu.pl>
2025-02-25 00:51:43 +0100

    class ServiceServerImpl remote implentation

---
+++ src/main/java/org/example/server/multithreading/servlets/ServiceServerImpl.java
@@ -0,0 +1,6 @@
+package org.example.server.multithreading.servlets;
+import java.rmi.*;
+import java.util.*;
+import java.rmi.server.*;
+
+public class ServiceServerImpl extends UnicastRemoteObject implements ServiceServer { }
