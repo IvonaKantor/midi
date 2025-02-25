4a580f023c7106c90588ded73d5ae37172d838dc
ivona <ik87056@student.uwb.edu.pl>
2025-02-25 16:32:33 +0100

    updating ServiceServer interface

--- src/main/java/org/example/server/multithreading/servlets/ServiceServer.java
+++ src/main/java/org/example/server/multithreading/servlets/ServiceServer.java
@@ -1,8 +1,9 @@
 package org.example.server.multithreading.servlets;
+
 import java.rmi.*;
-import java.security.Provider;
 
 public interface ServiceServer extends Remote {
     Object[] getServices() throws RemoteException;
-    Provider.Service getService(Object name) throws RemoteException;
+
+    Service getService(Object name) throws RemoteException;
 }
