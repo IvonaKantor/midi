81615737f78515e2aa5f2afd91da68642bcf5bef
ivona <ik87056@student.uwb.edu.pl>
2025-02-27 20:28:15 +0100

    updating MiniMusic service

--- src/main/java/org/example/server/multithreading/servlets/MiniMusicService.java
+++ src/main/java/org/example/server/multithreading/servlets/MiniMusicService.java
@@ -49,5 +49,15 @@ public class MiniMusicService implements Service {
         }
     }
 
-    public MidiEvent makeEvent(int comd, int chan, int one, int two, int tick) {}
+    public MidiEvent makeEvent(int comd, int chan, int one, int two, int tick) {
+        MidiEvent event = null;
+        try{
+            ShortMessage a = new ShortMessage();
+            a.setMessage(comd, chan, one, two);
+            event = new MidiEvent(a,tick);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return event;
+    }
 }
