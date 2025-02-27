be0667d1daf89f249f6d0d77482ec0c92ef1a70b
ivona <ik87056@student.uwb.edu.pl>
2025-02-27 19:44:20 +0100

    the most important method, returns jpanel widgets

--- src/main/java/org/example/server/multithreading/servlets/Diceservice.java
+++ src/main/java/org/example/server/multithreading/servlets/Diceservice.java
@@ -8,5 +8,20 @@ public class Diceservice implements Service{
     JLabel label;
     JComboBox numOfDice;
 
-    public JPanel getGuiPanel(){}
+    public JPanel getGuiPanel(){
+        JPanel panel = new JPanel();
+        JButton button = new JButton("Roll 'em!");
+        String[] choices = {"1", "2", "3", "4", "5"};
+        numOfDice = new JComboBox(choices);
+        label = new JLabel("dice values here");
+        button.addActionListener(new RollEmListener());
+        panel.add(numOfDice);
+        panel.add(button);
+        panel.add(label);
+        return panel;
+    }
+
+    public class RollEmListener implements ActionListener{
+        public void actionPerformed(ActionEvent e){}
+    }
 }
