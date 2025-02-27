2ce2897eb463de351df2e6dc03b91d5ca4100323
ivona <ik87056@student.uwb.edu.pl>
2025-02-27 19:59:01 +0100

    created MiniMusic service

---
+++ src/main/java/org/example/server/multithreading/servlets/MiniMusicService.java
@@ -0,0 +1,12 @@
+package org.example.server.multithreading.servlets;
+import org.example.beatB.MyDrawPanel;
+
+import javax.sound.midi.*;
+import java.io.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class MiniMusicService implements Service{
+    MyDrawPanel myPanel;
+}
