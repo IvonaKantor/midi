cecb0b5c16d90d6bea5454371ea101cf4b31f075
ivona <ik87056@student.uwb.edu.pl>
2025-03-01 22:50:13 +0100

    updating MiniMusic service

--- src/main/java/org/example/server/multithreading/servlets/ServiceBrowser.java
+++ src/main/java/org/example/server/multithreading/servlets/ServiceBrowser.java
@@ -11,6 +11,32 @@ public class ServiceBrowser {
     JComboBox serviceList;
     ServiceServer server;
 
+    public void buildGUI() {
+        JFrame frame = new JFrame("RMI Browser");
+        mainPanel = new JPanel();
+        frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
+
+        Object[] services = (Object[]) getServicesList();
+
+        serviceList = new JComboBox(services);
+        frame.getContentPane().add(BorderLayout.NORTH, serviceList);
+        serviceList.addItemListener((ItemListener) new MyListListener());
+        frame.setSize(500, 500);
+        frame.setVisible(true);
+    }
+
+    void loadService(Object serviceSelection) {
+        try {
+            Service svc = server.getService(serviceSelection);
+
+            mainPanel.removeAll();
+            mainPanel.add(svc.getGuiPanel());
+            mainPanel.validate();
+            mainPanel.repaint();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
 
 
