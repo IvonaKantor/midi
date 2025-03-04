27e7749e715898a5f469fefd0640b033a297fd20
ivona <ik87056@student.uwb.edu.pl>
2025-03-04 18:29:47 +0100

    universal service implementing Service

---
+++ src/main/java/org/example/server/multithreading/servlets/DayOfTheWeekService.java
@@ -0,0 +1,11 @@
+package org.example.server.multithreading.servlets;
+
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+
+public class DayOfTheWeekService {
+}
