114e384464a2f58ac72d3bf887c31a27cada824a
ivona <ik87056@student.uwb.edu.pl>
2025-03-06 00:20:08 +0100

    updated version of MusicSercer

--- src/main/java/org/example/server/multithreading/server/MusicServer.java
+++ src/main/java/org/example/server/multithreading/server/MusicServer.java
@@ -4,12 +4,12 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 
-public class MusicSercer {
+public class MusicServer {
 
     ArrayList<ObjectOutputStream> clientOutputStreams;
 
     public static void main(String[] args) {
-        new MusicSercer().go();
+        new MusicServer().go();
     }
 
     public class ClientHandler implements Runnable {
@@ -18,19 +18,19 @@ public class MusicSercer {
         Socket clientSocket;
 
         public ClientHandler(Socket socket) {
-            try{
+            try {
                 clientSocket = socket;
                 in = new ObjectInputStream(clientSocket.getInputStream());
-            }catch(Exception e){
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
 
-        public void run(){
-            Object o2 = null;
-            Object o1 = null;
-            try{
-                while((o1 = in.readObject()) != null){
+        public void run() {
+            Object o2;
+            Object o1;
+            try {
+                while ((o1 = in.readObject()) != null) {
                     o2 = in.readObject();
 
                     System.out.println("read two objects");
@@ -42,13 +42,13 @@ public class MusicSercer {
         }
     }
 
-    public void go(){
+    public void go() {
         clientOutputStreams = new ArrayList<>();
 
-        try{
+        try {
             ServerSocket serverSock = new ServerSocket(4242);
 
-            while(true){
+            while (true) {
                 Socket clientSocket = serverSock.accept();
                 ObjectOutputStream out = new ObjectOutputStream(clientSocket.getOutputStream());
                 clientOutputStreams.add(out);
@@ -58,19 +58,19 @@ public class MusicSercer {
 
                 System.out.println("got a connection");
             }
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public void tellEveryone(Object one, Object two){
+    public void tellEveryone(Object one, Object two) {
         Iterator iter = clientOutputStreams.iterator();
-        while(iter.hasNext()){
-            try{
-                ObjectOutputStream out = (ObjectOutputStream)iter.next();
+        while (iter.hasNext()) {
+            try {
+                ObjectOutputStream out = (ObjectOutputStream) iter.next();
                 out.writeObject(one);
                 out.writeObject(two);
-            }catch (Exception e){
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
