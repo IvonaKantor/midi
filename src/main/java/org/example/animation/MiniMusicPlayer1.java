d6f7ae2bf3fa0eaf717249c0609bd9e424411888
ivona <ik87056@student.uwb.edu.pl>
2025-03-10 17:20:02 +0100

    deleted wrapping in creating new sequences

--- src/main/java/org/example/animation/MiniMusicPlayer1.java
+++ src/main/java/org/example/animation/MiniMusicPlayer1.java
@@ -1,35 +1,37 @@
 package org.example.animation;
+
 import javax.sound.midi.*;
 
 public class MiniMusicPlayer1 {
     public static void main(String[] args) {
-        try{
+        try {
             Sequencer sequencer = MidiSystem.getSequencer();
             sequencer.open();
 
             Sequence seq = new Sequence(Sequence.PPQ, 4);
-            Track track = ((Sequence) seq).createTrack();
+            Track track = seq.createTrack();
 
-            for(int i = 5; i < 61; i+=4){
-                track.add(makeEvent(144,1,i,100,i));
-                track.add(makeEvent(128,1,i,100,i+2));
+            for (int i = 5; i < 61; i += 4) {
+                track.add(makeEvent(144, 1, i, 100, i));
+                track.add(makeEvent(128, 1, i, 100, i + 2));
             }
 
             sequencer.setSequence((Sequence) seq);
             sequencer.setTempoInBPM(220);
             sequencer.start();
-        }catch (Exception ex){
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
 
-    public static MidiEvent makeEvent(int comd, int chan, int one, int two, int tick){
+    public static MidiEvent makeEvent(int cmd, int chan, int one, int two, int tick) {
         MidiEvent event = null;
-        try{
+        try {
             ShortMessage a = new ShortMessage();
-            a.setMessage(comd, chan, one, two);
+            a.setMessage(cmd, chan, one, two);
             event = new MidiEvent(a, tick);
-        }catch(Exception ex){ }
+        } catch (Exception ignored) {
+        }
         return event;
     }
 }
