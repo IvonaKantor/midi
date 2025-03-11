7982402c242f1be17ee1dc3d0ec2d94546aed501
ivona <ik87056@student.uwb.edu.pl>
2025-03-11 17:29:17 +0100

    changed except handling

--- src/main/java/org/example/animation/Animate.java
+++ src/main/java/org/example/animation/Animate.java
@@ -1,4 +1,5 @@
 package org.example.animation;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -7,34 +8,35 @@ public class Animate {
     int x = 1;
     int y = 1;
 
-    public static void main(String[] argc){
+    public static void main(String[] argc) {
         Animate gui = new Animate();
         gui.go();
     }
 
-    public void go(){
+    public void go() {
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         MyDrawP drawP = new MyDrawP();
         frame.getContentPane().add(drawP);
-        frame.setSize(500,270);
+        frame.setSize(500, 270);
         frame.setVisible(true);
 
-        for(int i = 0; i < 124; i ++,x++,y++) {
+        for (int i = 0; i < 124; i++, x++, y++) {
             x++;
             drawP.repaint();
-            try{
+            try {
                 Thread.sleep(50);
-            }catch(Exception e){ }
+            } catch (Exception ignored) {
+            }
         }
     }
 
-    public class MyDrawP extends JPanel{
-        public void paintComponent(Graphics g){
+    public class MyDrawP extends JPanel {
+        public void paintComponent(Graphics g) {
             g.setColor(Color.white);
-            g.fillRect(0,0,500,250);
+            g.fillRect(0, 0, 500, 250);
             g.setColor(Color.blue);
-            g.fillRect(x,y,500-x*2,250-y*2);
+            g.fillRect(x, y, 500 - x * 2, 250 - y * 2);
         }
     }
 }
