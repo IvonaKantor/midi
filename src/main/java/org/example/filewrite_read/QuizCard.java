eb17deef57665ea3232dc2b45449ea21bcf032e1
ivona <ik87056@student.uwb.edu.pl>
2025-03-14 20:45:36 +0100

    new class for quizcard game

---
+++ src/main/java/org/example/filewrite_read/QuizCard.java
@@ -0,0 +1,15 @@
+package org.example.filewrite_read;
+
+public class QuizCard {
+
+    public QuizCard(String text, String text1) {
+    }
+
+    public String getQuestion() {
+        return String.valueOf(0);
+    }
+
+    public String getAnswer() {
+        return String.valueOf(0);
+    }
+}
