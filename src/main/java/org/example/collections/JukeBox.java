f419d25056df794a750156bc90ec24cdedf97bf6
ivona <ik87056@student.uwb.edu.pl>
2025-03-14 22:56:58 +0100

    updating

--- src/main/java/org/example/collections/JukeBox.java
+++ src/main/java/org/example/collections/JukeBox.java
@@ -1,43 +1,45 @@
 package org.example.collections;
+
 import java.util.*;
 import java.io.*;
 
 public class JukeBox {
-    ArrayList<Song> songList = new ArrayList<Song>();
+    ArrayList<Song> songList = new ArrayList<>();
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         new JukeBox().go();
     }
 
-    static class ArtistCompare implements Comparator<Song>{
-        public int compare(Song one, Song two){
+    static class ArtistCompare implements Comparator<Song> {
+        public int compare(Song one, Song two) {
             return one.getArtist().compareTo(two.getArtist());
         }
     }
 
-    public void go(){
+    public void go() {
         getSongs();
         System.out.println(songList);
         Collections.sort(songList);
         System.out.println(songList);
 
-        TreeSet<Song> songSet = new TreeSet<Song>(songList);
+        TreeSet<Song> songSet = new TreeSet<>(songList);
         System.out.println(songSet);
     }
 
-    void getSongs(){
-        try{
+    void getSongs() {
+        try {
             File file = new File("SongList.txt");
             BufferedReader reader = new BufferedReader(new FileReader(file));
-            String line = null;
-            while((line = reader.readLine()) != null){
+            String line;
+            while ((line = reader.readLine()) != null) {
                 addSong(line);
             }
-        }catch (Exception e){
-            e.printStackTrace(); }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    void addSong(String lineToParse){
+    void addSong(String lineToParse) {
         String[] tokens = lineToParse.split("/");
         Song nextSong = new Song(tokens[0], tokens[1], tokens[2], tokens[3]);
         songList.add(nextSong);
