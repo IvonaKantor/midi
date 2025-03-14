141f3ef95ba512d9c9d41b20b28848ae34e68d52
ivona <ik87056@student.uwb.edu.pl>
2025-03-14 22:44:03 +0100

    updating of the file

--- src/main/java/org/example/collections/Song.java
+++ src/main/java/org/example/collections/Song.java
@@ -1,17 +1,17 @@
 package org.example.collections;
 
-public class Song implements Comparable<Song>{
+public class Song implements Comparable<Song> {
     String title;
     String artist;
     String rating;
     String bpm;
 
-    public boolean equals(Object aSong){
+    public boolean equals(Object aSong) {
         Song s = (Song) aSong;
         return getTitle().equals(s.getTitle());
     }
 
-    public int hashCode(){
+    public int hashCode() {
         return getTitle().hashCode();
     }
 
@@ -26,23 +26,23 @@ public class Song implements Comparable<Song>{
         bpm = b;
     }
 
-    public String getTitle(){
+    public String getTitle() {
         return title;
     }
 
-    public String getArtist(){
+    public String getArtist() {
         return artist;
     }
 
-    public String getRating(){
+    public String getRating() {
         return rating;
     }
 
-    public String getBPM(){
+    public String getBPM() {
         return bpm;
     }
 
-    public String toString(){
+    public String toString() {
         return title;
     }
 }
