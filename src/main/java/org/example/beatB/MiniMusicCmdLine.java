ea9d57461f9783dd1ed3ab0a9f83ef3a6d3214b4
ivona <ik87056@student.uwb.edu.pl>
2025-03-14 22:32:47 +0100

    deleted null initialization of variable

--- src/main/java/org/example/beatB/MiniMusicCmdLine.java
+++ src/main/java/org/example/beatB/MiniMusicCmdLine.java
@@ -1,11 +1,12 @@
 package org.example.beatB;
+
 import javax.sound.midi.*;
 
 public class MiniMusicCmdLine {
 
     public static void main(String[] args) {
         MiniMusicCmdLine mini = new MiniMusicCmdLine();
-        if(args.length < 2)
+        if (args.length < 2)
             System.out.println("Don't forget about arguments ");
         else {
             int instrument = Integer.parseInt(args[0]);
@@ -16,7 +17,7 @@ public class MiniMusicCmdLine {
 
     public void play(int instrument, int note) {
 
-        try{
+        try {
             Sequencer player = MidiSystem.getSequencer();
             player.open();
 
@@ -24,26 +25,24 @@ public class MiniMusicCmdLine {
 
             Track track = seq.createTrack();
 
-            MidiEvent event = null;
-
             ShortMessage first = new ShortMessage();
-            first.setMessage(192,1, instrument,0);
-            MidiEvent changeInstrument = new MidiEvent(first,1);
+            first.setMessage(192, 1, instrument, 0);
+            MidiEvent changeInstrument = new MidiEvent(first, 1);
             track.add(changeInstrument);
 
             ShortMessage a = new ShortMessage();
-            a.setMessage(144,1,note,100);
-            MidiEvent noteOn = new MidiEvent(a,1);
+            a.setMessage(144, 1, note, 100);
+            MidiEvent noteOn = new MidiEvent(a, 1);
             track.add(noteOn);
 
             ShortMessage b = new ShortMessage();
-            b.setMessage(128,1,note,100);
-            MidiEvent noteOff = new MidiEvent(b,16);
+            b.setMessage(128, 1, note, 100);
+            MidiEvent noteOff = new MidiEvent(b, 16);
             track.add(noteOff);
             player.setSequence(seq);
             player.start();
 
-        }catch(Exception ex){
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
