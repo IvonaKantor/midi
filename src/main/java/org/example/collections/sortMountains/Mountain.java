110c01325f1c976ad149482c650d5a3e8b52513e
ivona <ik87056@student.uwb.edu.pl>
2025-03-14 22:45:53 +0100

    changed a logic of the methods in the class

--- src/main/java/org/example/collections/sortMountains/Mountain.java
+++ src/main/java/org/example/collections/sortMountains/Mountain.java
@@ -3,12 +3,12 @@ package org.example.collections.sortMountains;
 public class Mountain {
     String name;
     int height;
-    
+
     Mountain(String name, int length) {
         this.name = name;
         this.height = length;
     }
-    
+
     public String toString() {
         return "Mountain [name=" + name + ", length=" + height + "]";
     }
