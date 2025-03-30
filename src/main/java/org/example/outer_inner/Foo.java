270e681b87aa26d379584f56586c8d34f79039aa
ivona <ik87056@student.uwb.edu.pl>
2025-03-30 23:42:19 +0200

    added calling outer class function

--- src/main/java/org/example/outer_inner/Foo.java
+++ src/main/java/org/example/outer_inner/Foo.java
@@ -4,5 +4,8 @@ public class Foo {
     public static void main(String[] argc) {
         MyOuterClass outerobj = new MyOuterClass();
         MyOuterClass.MyInnerClass innerobj = new MyOuterClass.MyInnerClass();
+
+        outerobj.inner = innerobj;
+        innerobj.go();
     }
 }
