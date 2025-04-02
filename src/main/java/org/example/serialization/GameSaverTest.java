fcb6407ff52432646e42c5ef32475a26547ecd74
ivona <ik87056@student.uwb.edu.pl>
2025-04-02 14:06:18 +0200

    updating

--- src/main/java/org/example/serialization/GameSaverTest.java
+++ src/main/java/org/example/serialization/GameSaverTest.java
@@ -1,26 +1,27 @@
 package org.example.serialization;
+
 import java.io.*;
 
 public class GameSaverTest {
-    public static void main(String[] argc){
+    public static void main(String[] argc) {
         GameCharacter one = new GameCharacter(50, "elf", new String[]{"bow", "sword", "brass knuckles"});
         GameCharacter two = new GameCharacter(50, "troll", new String[]{"bare hands", "hatchet"});
         GameCharacter three = new GameCharacter(50, "magician", new String[]{"spell", "invisibility"});
 
-        try{
+        try {
             ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream("Game.ser"));
             os.writeObject(one);
             os.writeObject(two);
             os.writeObject(three);
             os.close();
-        }catch(IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
         one = null;
         two = null;
         three = null;
 
-        try{
+        try {
             ObjectInputStream is = new ObjectInputStream(new FileInputStream("Game.ser"));
             GameCharacter oneRestore = (GameCharacter) is.readObject();
             GameCharacter twoRestore = (GameCharacter) is.readObject();
@@ -29,7 +30,7 @@ public class GameSaverTest {
             System.out.println("Type of the first character: " + oneRestore.getType());
             System.out.println("Type of the second character: " + twoRestore.getType());
             System.out.println("Type of the third character: " + threeRestore.getType());
-        }catch(Exception ex){
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
