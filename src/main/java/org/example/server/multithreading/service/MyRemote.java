0055a6d7aedd1a909c01cfb4f817246fdb5c5871
ivona <ik87056@student.uwb.edu.pl>
2025-04-11 19:22:38 +0200

    small service for client

--- src/main/java/org/example/server/multithreading/service/MyRemote.java
+++ src/main/java/org/example/server/multithreading/service/MyRemote.java
@@ -4,5 +4,5 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface MyRemote extends Remote {
-    public String hello() throws RemoteException;
+    String hello() throws RemoteException;
 }
