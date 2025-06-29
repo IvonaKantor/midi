3c2519ac519212045e767126d7240d80ec600c0b
ivona <ik87056@student.uwb.edu.pl>
2025-06-29 23:42:57 +0200

    updating

--- src/main/java/org/example/server/multithreading/withdrawal/TestSync.java
+++ src/main/java/org/example/server/multithreading/withdrawal/TestSync.java
@@ -4,14 +4,14 @@ public class TestSync implements Runnable {
 
     private int balance;
 
-    public void run(){
-        for(int i = 0; i < 50; i++){
+    public void run() {
+        for (int i = 0; i < 50; i++) {
             increment();
             System.out.println("balance:" + balance);
         }
     }
 
-    public synchronized void increment(){
+    public synchronized void increment() {
         int i = balance;
         balance = i + 1;
     }
